582d93d738e7e1e280e7ffc81b722fbdd2e8b458
Sebastian Pfeiler <spfeiler@student.ehz.ch>
2021-11-12 12:06:35 +0100

    Did the thing

---
+++ C++/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "vector.h"
+#include <chrono>
+
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
+
+int main(){
+	constexpr size_t iterations = 200000;
+	auto t0 = high_resolution_clock::now();
+	std::cout <<  "Dimensions, Total Time, Average Time" << std::endl;
+	for(size_t i = 2; i <200; i += 2){
+		double average = 0;
+		for(size_t j = 0; j < iterations; j++){ 
+			f_vector v1 = generateRandomVector(i); // 1/4 kB
+			f_vector v2 = generateRandomVector(i); // 1/4 kB
+			auto t1 = high_resolution_clock::now(); // I expect this to be a non factor
+			float angle = angleBetween(v1,v2);
+			__asm__ __volatile__ ("" : : : "memory"); // Evil Vodoo magic
+			auto t2 = high_resolution_clock::now();
+			duration<double, std::milli> dur = t2 - t1;
+			average += dur.count();
+		}
+		////average /= iterations;
+		std::cout << i << "," << average << "," << average/iterations << std::endl;
+	}
+
+	duration<double, std::milli> total = high_resolution_clock::now() - t0;
+	std::cout << "Total time: " << total.count() << std::endl;
+
+
+}
---
+++ C++/vector.h
@@ -0,0 +1,50 @@
+#ifndef Vector
+#define Vector
+
+#include <cstdlib>
+#include <math.h>
+#include <cassert>
+#include <stdlib.h>
+#include <time.h>
+
+struct f_vector{
+	float *e;
+	size_t dimensions;
+};
+
+
+f_vector generateRandomVector(size_t dim){
+	f_vector v = {new float[dim], dim};
+	srand(time(NULL));		
+	for(int i = 0; i < v.dimensions; i++){
+		int j = rand();
+		v.e[i] = *((float*)&j);
+
+	}
+	return v;
+}
+
+float dotProductSimple(f_vector v1, f_vector v2){
+	assert(v1.dimensions = v2.dimensions);
+	float sum = 0;
+	for(int i = 0; i < v1.dimensions; i++){
+		sum += v1.e[i] * v2.e[i];
+	}
+	return sum;
+}
+
+float norm(f_vector v){
+	float sum = 0;
+	for(int i = 0; i < v.dimensions; i++){
+		sum += v.e[i] * v.e[i]; 
+	}
+	return sqrt(sum);
+}
+float angleBetween(f_vector v1, f_vector v2){
+	float v1Norm = norm(v1);
+	float v2Norm = norm(v2);
+	float dotV1V2 = dotProductSimple(v1,v2);
+	return acos(dotV1V2/(v1Norm * v2Norm));
+}
+
+#endif 
