48155c8342119c7f23e504ef87e6caca02f71c4b
Patrick O'Neill <poneill2016@gmail.com>
2021-11-13 22:40:31 -0500

    add clobber function documentation

--- C++/main.cpp
+++ C++/main.cpp
@@ -10,21 +10,22 @@ using std::chrono::duration_cast;
 using std::chrono::duration;
 using std::chrono::milliseconds;
 
-void static 
-escape(void *p) // Mr. Carruth is asking you to use this function only for good ;D
+void static escape(void *p) // Mr. Carruth is asking you to use this function only for good ;D
 {
 	asm volatile("" : : "g"(p): "memory");
 }
 
-void static 
-clobber()
+/*
+Act as a compiler fence to ensure that the optimizer does not optimize instructions before or after this instruction.
+This prevents instruction scrambling that could cause the end time to be recorded before the calculation function is
+actually complete.
+*/
+void static clobber()
 {
 	asm volatile("" : : : "memory");
 }
 
-
-void
-testDimensionsFromTo(
+void testDimensionsFromTo(
 		float (*angleCalculator)(f_vector, f_vector)
 		, size_t end = 100
 		, size_t step = 10
@@ -51,8 +52,7 @@ testDimensionsFromTo(
 	}
 }
 
-void
-testDimensionsFromToPreGeneratedVectors(
+void testDimensionsFromToPreGeneratedVectors(
 		float (*angleCalculator)(f_vector, f_vector)
 		, size_t end = 100
 		, size_t step = 10
@@ -82,19 +82,16 @@ testDimensionsFromToPreGeneratedVectors(
 		}
 		duration<double, std::milli> dur = high_resolution_clock::now() - t1;
 		free(arrayOfVectors);
-		std::cout 
-			<< "," << i 
-			<< "," << dur.count() 
-			<< "," << dur.count()/iterations_per_dimensions << std::endl;
+		std::cout << i << ","
+		          << dur.count() << ","
+			      << dur.count()/iterations_per_dimensions << std::endl;
 	}
 }
 
-
 // This is a macro for easier benchmarking
 #define RUN_TEST(function, parameters)					\
-	std::cout << "Running " << STRINGYFY(function) << std::endl; 	\
+	std::cout << "Running " << STRINGYFY(function) << ",," << std::endl; 	\
 	function parameters;
-
 
 int main(void){
 	RUN_TEST(testDimensionsFromTo, (angleBetweenNonNormalized,100,5,200000))
